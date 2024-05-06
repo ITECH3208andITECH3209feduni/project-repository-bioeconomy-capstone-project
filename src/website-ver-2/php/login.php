4c5673aa571d0a0dd3488e4602df2de40f928193
mlangeles <migznvidia@gmail.com>
2024-05-06 02:26:03 +0000

    Website Prototype

---
+++ src/website-ver-2/php/login.php
@@ -0,0 +1,24 @@
+<?php
+$host = "db"; 
+$port = "5432";
+$dbname = "your_database_name";
+$user = "your_postgres_user";
+$password = "your_postgres_password";
+
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+
+header("location:../index.html");
+
+// Function to sanitize user input
+function sanitize_input($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+pg_close($conn);
+
+echo "Connection closed.";
+?>
