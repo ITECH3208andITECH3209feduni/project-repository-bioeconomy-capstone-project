8cf0091c5b297e824c1ec516c41414a05f3a7e7e
mlangeles <migznvidia@gmail.com>
2024-05-15 01:55:56 +0000

    Added Django Docs

--- src/mysite/polls/urls.py
+++ src/mysite/polls/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-from . import views
+from .views import login
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('login/', login, name='login'),
 ]
--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def login(request):
+    # Your login view logic here
+    return render(request=request, template_name='login.html')
