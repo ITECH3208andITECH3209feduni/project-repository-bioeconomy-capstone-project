b474ccdd21991723c3954e6e07f55d8f437ab637
mlangeles <migznvidia@gmail.com>
2024-05-20 06:39:57 +0000

    Implemented Authentication

--- src/mysite/polls/urls.py
+++ src/mysite/polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import login
+from .views import login_view
+from .views import success_view
+from .views import logout_view
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', login_view, name='login'),
+    path('success/', success_view, name='success'),
+    path('logout/', logout_view, name='logout'),
 ]
--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-def login(request):
-    # Your login view logic here
-    return render(request=request, template_name='login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')  # Use .get() to avoid KeyError
+        password = request.POST.get('password', '')  # Use .get() to avoid KeyError
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/polls/success')  # Redirect to a success page or dashboard
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
+    
+@login_required
+def success_view(request):
+    return render(request, 'success.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
