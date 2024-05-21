0d861e2c31d614262f7a216d2d23773e1a623c45
PeterM1004100 <141429729+PeterM1004100@users.noreply.github.com>
2024-05-21 14:20:33 +1000

    Update login.js
    Updated login

--- src/website-ver-2/js/login.js
+++ src/website-ver-2/js/login.js
@@ -1,22 +1,59 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+(function ($) {
+    "use strict";
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
+    // Spinner Function
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
+    // Initiate WOW.js
+    new WOW().init();
 
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
 
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-    wrapper.classList.remove('active');
-});
+    // Back to Top Button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Facts Counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+    // Testimonials Carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+})(jQuery);
