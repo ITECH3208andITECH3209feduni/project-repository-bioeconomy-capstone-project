17c559e303a46742f7e3db210b49df21c1dcca5a
mlangeles <77595650+mlangeles@users.noreply.github.com>
2024-06-16 13:12:45 +1000

    Delete src/mysite directory

--- src/mysite/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
-    path('upload/', views.upload_file, name='upload_file'),
-    path('api/waste-data/', views.get_waste_data, name='get_waste_data'),
-]
